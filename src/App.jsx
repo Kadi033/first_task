a707e72d7959f61aae45a5b392fa2c59da91e638
Kadi033 <demetre33dev@gmail.com>
2024-09-23 19:55:09 +0400

    i change path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
-import Header from "./components/header/header";
-import Main from "./components/main/main";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 function App() {
   return (
     <>
